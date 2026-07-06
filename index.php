4af5557d1a292ad8c1b3b5605a463109417060e8
priyanshumishra610 <priyanshum8874@gmail.com>
2026-07-06 10:29:02 +0530

    Dental Clinic Webpage Made

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+date_default_timezone_set('Asia/Kolkata');
+
 $conn = mysqli_connect('localhost','root','','contact_db') or die('connection failed');
 
 if(isset($_POST['submit'])){
@@ -9,12 +11,16 @@ if(isset($_POST['submit'])){
    $number = $_POST['number'];
    $date = $_POST['date'];
 
-   $insert = mysqli_query($conn, "INSERT INTO `contact_form`(name, email, number, date) VALUES('$name','$email','$number','$date')") or die('query failed');
-
-   if($insert){
-      $message[] = 'appointment made successfully!';
+   if(!preg_match('/^[6-9]\d{9}$/', $number)){
+      $message[] = 'Please enter a valid 10-digit Indian mobile number.';
    }else{
-      $message[] = 'appointment failed';
+      $insert = mysqli_query($conn, "INSERT INTO `contact_form`(name, email, number, date) VALUES('$name','$email','$number','$date')") or die('query failed');
+
+      if($insert){
+         $message[] = 'Appointment booked successfully! We will contact you shortly.';
+      }else{
+         $message[] = 'Appointment booking failed. Please try again or call us.';
+      }
    }
 
 }
